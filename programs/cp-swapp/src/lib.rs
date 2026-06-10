5165004f8561684c3de4f5205218073d2a4c6fc4
Yashwant_Ostwal <yashwant08012005@gmail.com>
2026-06-11 01:26:15 +0530

    Deposit ixn

--- programs/cp-swapp/src/lib.rs
+++ programs/cp-swapp/src/lib.rs
@@ -2,12 +2,14 @@ pub mod constants;
 pub mod error;
 pub mod instructions;
 pub mod states;
+pub mod utils;
 
 use anchor_lang::prelude::*;
 
 pub use constants::*;
 pub use instructions::*;
 pub use states::*;
+pub use utils::*;
 
 declare_id!("7SghMcfPqxKxVbrpMtEJ7rR24g2Cjbzm82C14wo3pqFy");
 
@@ -18,7 +20,7 @@ pub mod cp_swapp {
     // pub fn initialize(ctx: Context<Initialize>,fees_in_ppm:u32,net_init_amount_a:u64,net_init_amount_b:u64) -> Result<()> {
     //     instructions::initialize_handler(ctx, fees_in_ppm,net_init_amount_a, net_init_amount_b)
     // }
-    pub fn deposit(ctx: Context<Deposit>,fees_in_ppm:u32) -> Result<()> {
-        instructions::deposit_handler(ctx, fees_in_ppm)
+    pub fn deposit(ctx: Context<Deposit>,fees_in_ppm:u32,lp_tokens_required:u64,max_deposit_a:u64,max_deposit_b:u64) -> Result<()> {
+        instructions::deposit_handler(ctx, fees_in_ppm,lp_tokens_required,max_deposit_a,max_deposit_b)
     }
 }
